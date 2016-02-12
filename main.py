2f14f3182da120b7c8e2cad9c2b5e2717e9ac97d
francis maina <frankmaina@live.com>
2016-02-12 21:12:59 +0300

    Updated Warning

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 """
-Warning: This project and code entailed in this  project is in
-no way production quality or is it either intended to be used in
-actual prediction with real life stakes.
+Warning: This project and its original source code have been created out of hobby,
+and are free to download and use, therefore of non-profit nature.
+
+The Author of this site is not affiliated,
+or connected in any other way, with any of the external sites linked from this project.
+Their presence on this project is shear act of sharing information related to sports prediction
+and betting activities.
+
+Therefore, Webmaster of this site cannot guarantee accuracy of any information
+or calculation acquierd from using the project , and does not accept any responsibility for
+any losses or damages you may suffer due to the use.
+
+Please bear in mind that sports betting is sensible act,
+that may lead you to losing significant money and time,
+and/or may lead you to addiction. You should never expose
+more money to sports trading and betting than you can afford
+to lose, and you should never dedicate time to sports trading a
+nd betting when you need to be involved in other personal
+ or business activities.
 """
