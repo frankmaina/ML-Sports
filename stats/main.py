ac3b003dce229da7d8bd905f3c68358ccc8dfdbb
francis maina <frankmaina@live.com>
2016-02-20 19:23:20 +0300

    Starting on prediction engine

--- stats/main.py
+++ stats/main.py
@@ -10,25 +10,25 @@ def odds_stats(odds):
     return {'H': hometeam_percentage, 'A': awayteam_percentage}
 
 
-class Match(object):
-    def get_best_odds(self, odds):
-        # return value of team most likely to win,or rather highest odds/chances(rarely a draw)
-        value = odds.index(min(odds))
-        if value == 0:
-            return ('H', value)
-        elif value == 1:
-            return ('D', value)
-        else:
-            return ('A', value)
+def get_best_odds(odds):
+    # return value of team most likely to win,or rather highest odds/chances(rarely a draw)
+    value = odds.index(min(odds))
+    if value == 0:
+        return ('H', value)
+    elif value == 1:
+        return ('D', value)
+    else:
+        return ('A', value)
 
-    def get_match_winner(self, actual_scores):
-        if actual_scores[0] > actual_scores[1]:
-            # the home team won
-            return {'W': 'H', 'L': 'A', 'D': 0}
-        elif actual_scores[1] > actual_scores[0]:
-            return {'W': 'A', 'L': 'H', 'D': 0}
-        else:
-            return {'W': 0, 'L': 0, 'D': 1}
+
+def get_match_winner(self, actual_scores):
+    if actual_scores[0] > actual_scores[1]:
+        # the home team won
+        return {'W': 'H', 'L': 'A', 'D': 0}
+    elif actual_scores[1] > actual_scores[0]:
+        return {'W': 'A', 'L': 'H', 'D': 0}
+    else:
+        return {'W': 0, 'L': 0, 'D': 1}
 
 
 def elo_rating_analytics(home_team, away_team):
