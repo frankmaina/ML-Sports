fce5e2bb7289fa30f558f6330ebbeafee10fa009
Francis Maina <frankmaina@live.com>
2016-02-29 08:27:02 +0300

    Changes

--- main.py
+++ main.py
@@ -28,21 +28,25 @@ Short odds: The likelihood of an event occurring is high.
 """
 
 
-from training.main import create_ratings
+from training.main import create_ratings,train_ratings
 from engine.main import *
 
 mode = "train"
 
 if mode=="predict":
 
+
+
     # prepare command line arguments passed in
-    hometeam = {'team_name': "abcd", 'upset_potential': 0, 'rating': 1359, 'games_played': 1}
-    awayteam = {'team_name': "abcd", 'upset_potential': 0, 'rating': 1200, 'games_played': 1}
+    hometeam = {'team_name': "Newcastle", 'upset_potential': 0, 'rating': 1084, 'games_played': 1}
+    awayteam = {'team_name': "Norwich", 'upset_potential': 0, 'rating': 1104, 'games_played': 1}
 
     odds = [11.95, 3.26, 1.84]
     actual_scores = [2, 1]
     # odds_percentage = odds_stats(odds)
-
+    apply_prediction(hometeam,awayteam,odds)
     apply_prediction(hometeam, awayteam, odds)
 elif mode == "train":
-    create_ratings('E0.csv')
+	'''create_ratings('E0.csv')'''
+	actual_scores = [0, 0]
+	train_ratings('E0.csv',actual_scores)
--- training/main.py
+++ training/main.py
@@ -1,4 +1,6 @@
 from peewee import *
+from stats.main import apply_elo_rating
+
 
 db = SqliteDatabase('mlsports.db')
 
@@ -34,5 +36,26 @@ def create_ratings(file_path):
             except Exception as e:
                 #the team does not exist in the db hence we create it
                 away_team  = TeamRating.create(name=row['AwayTeam'],rating=1000)
+        print 'Successfully created ratings for the provided teams.'
 
-         print 'Successfully created ratings for the provided teams.'
+
+
+def train_ratings(file_path,actual_scores):
+    import csv
+    with open(file_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            home_team_ratings = TeamRating.get(TeamRating.name==row['HomeTeam'])
+            away_team_ratings = TeamRating.get(TeamRating.name==row['AwayTeam'])
+            home_team = {'team':row['HomeTeam'],'rating':home_team_ratings.rating}
+            away_team = {'team':row['AwayTeam'],'rating':away_team_ratings.rating}
+            actual_scores[0] = row['FTHG']
+            actual_scores[1] = row['FTAG']
+            teams = apply_elo_rating(home_team,away_team,actual_scores)
+            print row['HomeTeam'], teams[0]
+            home_team = TeamRating.get(TeamRating.name==row['HomeTeam'])
+            home_team.rating = teams[0]
+            home_team.save()
+            away_team = TeamRating.get(TeamRating.name==row['AwayTeam'])
+            away_team.rating = teams[1]
+            away_team.save()
